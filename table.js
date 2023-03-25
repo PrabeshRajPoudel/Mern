3a2df49867fb0fbae080eed2d05acd0fa12ec29f
Prabesh <prabesh210@gmail.com>
2023-03-25 09:01:46 +0545

    table

---
+++ table.js
@@ -0,0 +1,22 @@
+// function table(index, first, last) {
+
+//     for (let i = first; i <= last; i++) {
+//         console.log(`${index} * ${i} =  ${index * i}`);
+//     }
+//     console.log("\n")
+// }
+// table(3, 5, 10)
+// table(4, 2, 6)
+// table(5, 7, 10)
+// table(6, 5, 20)
+let table = (index, first, last) => {
+
+    for (let i = first; i <= last; i++) {
+        console.log(`${index} * ${i} =  ${index * i}`);
+    }
+    console.log("\n")
+}
+table(3, 5, 10)
+table(4, 2, 6)
+table(5, 7, 10)
+table(6, 5, 20)
